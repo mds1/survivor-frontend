276f2b4832bbd991a850e0d72ba527dc981430bc
mds <17163988+mds1@users.noreply.github.com>
2018-08-26 17:32:59 -0400

    Updates for Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+// This file is needed for Heroku deployment
+const express = require('express');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(history());
+app.use(serveStatic(`${__dirname}/dist/spa-mat`));
+app.listen(port);
+
